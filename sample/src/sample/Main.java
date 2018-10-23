09c3d87bfb79d6a623a66ac9cd8f76c32c1f3cb2
justii1306 <35031460+justii1306@users.noreply.github.com>
2018-10-23 09:38:05 +0200

    Add files via upload

---
+++ sample/src/sample/Main.java
@@ -0,0 +1,15 @@
+package sample;
+
+        import javafx.application.Application;
+        import javafx.beans.value.ChangeListener;
+        import javafx.beans.value.ObservableValue;
+        import javafx.geometry.Insets;
+        import javafx.scene.Scene;
+        import javafx.scene.control.Slider;
+        import javafx.scene.control.ToggleButton;
+        import javafx.scene.layout.GridPane;
+
+public class Main extends Application{
+
+
+}
